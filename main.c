091fc13eeca76d2b8688ee50476a2561a38dce0a
Carlos <carlos@bespin.nayarsystems>
2017-12-18 16:16:36 +0100

    Aplicación calculadora finalizada

--- main.c
+++ main.c
@@ -3,17 +3,47 @@
 #include<stdio.h>
 
 int main(void) {
-	int num1, num2;
+	int num1, num2, opcion, resultado;
 	printf("*************************************\n");
 	printf("*CALCULADORA*************************\n");
 	printf("*************************************\n");
 
-	printf("Introduce un número ");
+	printf("Elige la operación que quieres hacer:\n");
+	printf("\t1. Sumar\n");
+	printf("\t2. Restar\n");
+	printf("\t3. Multiplicar\n");
+	printf("\t4. Dividir\n");
+	scanf("%d", &opcion);
+
+	if ( opcion < 1 || opcion > 4) {
+		printf("La opción que has elegido no es válida\n");
+		return 1;
+	}
+
+	printf("Introduce un número: ");
 	scanf("%d", &num1);
-	printf("Ahora introduce otro ");
+	printf("Ahora introduce otro: ");
 	scanf("%d", &num2);
 
-	int resultado = suma(num1, num2);
-	printf("El resultado de la suma es: ");
-	printf("%d\n", resultado);
+	switch(opcion) {
+		case 1:
+			resultado = suma(num1, num2);
+			printf("El resultado de la operación es: %d\n", resultado);
+			break;	
+		case 2:
+			resultado = resta(num1, num2);
+			printf("El resultado de la operación es: %d\n", resultado);
+			break;
+		case 3:	
+			resultado = multiplicacion(num1, num2);
+			printf("El resultado de la operación es: %d\n", resultado);
+			break;
+		case 4:
+			resultado = division(num1, num2);
+			printf("El resultado de la operación es: %d\n", resultado);
+			break;
+	}
+			
+
+	return 0;
 }
